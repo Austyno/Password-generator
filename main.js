fc16069c46e9ab97dcf572bfd57054c3a989e9fc
Austin Alozie <aloziebtc@gmail.com>
2019-03-22 02:35:41 +0100

    added database support

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow,Menu,ipcMain} = require('electron')
+
 // const menuTemplate = require('./lib/js/menu')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 let loginWindow;
+let urlWindow;
+
+
+//insert into db table pswd
+// db.tables.pswd.data = [   // You can mix SQL and JavaScript
+//   {one:3,two:4},
+//   {one:5,two:6},
+// ];
+
+    //read data from db
+// var res = db.exec("SELECT * FROM test ORDER BY two DESC");
+
+
 
 function createWindow () {
   // Create the browser window.
@@ -29,8 +43,18 @@ function createWindow () {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null,
-    loginWindow = null
+    loginWindow = null,
+    urlWindow = null
   })
+}
+
+function createUrlWindow(){
+  urlWindow = new BrowserWindow({
+    height:200,
+    width:200,
+    title: 'Url'
+  });
+  urlWindow.loadFile('windows/url.html');
 }
 
 function createLoginWindow(){
@@ -39,14 +63,13 @@ function createLoginWindow(){
     width:250,
     title: 'Login'
   });
-
-  loginWindow.loadFile('login.html');
-
+  loginWindow.loadFile('windows/login.html');
 };
 
 function save(){
-  console.log('saving...')
-  mainWindow.webContents.send('save');
+  console.log('saving...');
+  createUrlWindow(); 
+  // mainWindow.webContents.send('save');
 }
 
 // This method will be called when Electron has finished
@@ -83,6 +106,12 @@ ipcMain.on('master-paswd',(e,pass)=>{
   
 })
 
+//receive entered url from url window and send to mainWindow 
+ipcMain.on('url',(e,pswdUrl)=>{
+mainWindow.webContents.send('url',pswdUrl);
+urlWindow.close();
+})
+
 ipcMain.on('login-error',()=>{
   createLoginWindow();
 })
@@ -105,7 +134,6 @@ const menuTemplate = [
       }
     ]
   },
-
   {
     label: ' Dev Tools',
     submenu: [
@@ -125,13 +153,12 @@ const menuTemplate = [
         label : 'Saved PassWords',
         accelerator: 'CmdOrCtrl+g',
         click(){
-          createLoginWindow();
-          // mainWindow.webContents.send('saved-pass');
+          // createLoginWindow();
+          mainWindow.webContents.send('master-paswd');
         }
       }
     ]
   }
 ];
-
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
--- renderer.js
+++ renderer.js
@@ -1,12 +1,13 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
-const {ipcRenderer,shell} = require('electron');
+
+const {ipcRenderer,shell,clipboard} = require('electron');
 const helper = require('./lib/js/helper');
+const Datastore = require('nedb');
+const db = new Datastore({ filename: 'lib/db/password.db', autoload: true });
+
      
 const btn = document.getElementById('btn'),
-      passDisp = document.getElementById('password'),
-      num = Math.floor(Math.random()*(50-10+1)+10);
+      passDisp = document.getElementById('password');
+      // num = Math.floor(Math.random()*(50-10+1)+10);
       
 
       btn.addEventListener('click',(e)=>{
@@ -19,17 +20,38 @@ const btn = document.getElementById('btn'),
          }       
       })
 
+//receive entered url from main
+ipcRenderer.on('url',(e,pswdUrl)=>{
+  const pass = passDisp.value;
+
+   //insert into db 
+   const savePass = {
+     passWord : pass,
+     url : pswdUrl
+   }
+ db.insert(savePass,(error,newsavePass)=>{
+   console.log(newsavePass._id)
+ })
+ 
+  //  console.log(pswdUrl);
+  //  console.log(pass);
+})
 
 //open saved passwords
 ipcRenderer.on('saved-pass',()=>{
-  console.log('opening saved passwords....')
+  
+  console.log('opening saved passwords....');
 })
 
 ipcRenderer.on('master-paswd',(e,pass)=>{
-  pass = false;
-  //get stored passwords from DB
+  
+  
+  pass = true;
   if(pass){
-    console.log(pass)
+    
+    db.find({},(err,doc)=>{
+      console.log(doc);
+    })
   }else{
     shell.beep()
     alert('wrong login password')
@@ -40,5 +62,6 @@ ipcRenderer.on('master-paswd',(e,pass)=>{
 
 //save passwords to DB
 ipcRenderer.on('save',()=>{
-  console.log(passDisp.innerHTML)
+
+  // console.log(passDisp.innerHTML)
 })
